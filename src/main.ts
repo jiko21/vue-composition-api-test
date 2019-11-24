3d955e34214d5eaaef3de9fe9e392c65e7151354
jiko21 <daikikojima0531@icloud.com>
2019-11-24 15:29:02 +0900

    A: Composition API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue';
+import VueCompositionApi from '@vue/composition-api';
 import App from './App.vue';
 
+Vue.use(VueCompositionApi);
 Vue.config.productionTip = false;
 
 new Vue({
